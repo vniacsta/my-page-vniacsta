3a863a6c3bbce1fb65a0af480dc27b7b2b0ee33c
Vania Costa <vniacsta@users.noreply.github.com>
2020-11-30 16:12:53 +0000

    Changed pictures so they all match size

--- src/Projects.js
+++ src/Projects.js
@@ -11,55 +11,57 @@ export default function Projects() {
   return (
     <div className="Projects">
       <h3>Check out my projects so far:</h3>
-      <div className="row">
-        <div className="col-sm">
-          <ProjectsInfo
-            image={veganpower}
-            linkImage="https://veganpower.netlify.app"
-            name="Vegan Power"
-            linkProject="https://veganpower.netlify.app"
-            linkGithub="https://github.com/vniacsta/veganpower-vanilla-website"
-            />
-        </div>
-        <div className="col-sm">
-          <ProjectsInfo
-            image={weatherApp}
-            linkImage="https://react-app-project-vniacsta.netlify.app/"
-            name="Weather App React"
-            linkProject="https://react-app-project-vniacsta.netlify.app/"
-            linkGithub="https://github.com/vniacsta/react-app-project"
-            />
-        </div>
-        <div className="col-sm">
-          <ProjectsInfo
-            image={quizApp}
-            linkImage="https://quiz-app-javascript-vniacsta.netlify.app/"
-            name="Quiz App Javascript"
-            linkProject="https://quiz-app-javascript-vniacsta.netlify.app/"
-            linkGithub="https://github.com/vniacsta/js-quiz-lordoftherings"
-            />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm">
-          <ProjectsInfo
-            image={calcularhoras}
-            linkImage="https://calcularhoras.pt"
-            name="Calcular Horas"
-            linkProject="https://calcularhoras.pt"
-            linkGithub="https://github.com/vniacsta/calculate-hours"
-            />
-        </div> 
-        <div className="col-sm">
-          <ProjectsInfo
-            image={aecrru}
-            linkImage="https://www.aecrru.pt/"
-            name="AECRRU | Construction company website"
-            linkProject="https://www.aecrru.pt/"
-            linkGithub="https://github.com/vniacsta/js-website-aecrru"
-            />
-        </div> 
-      </div>
+      <table>
+        <tr>
+          <td>
+            <ProjectsInfo
+              image={veganpower}
+              linkImage="https://veganpower.netlify.app"
+              name="Vegan Power"
+              linkProject="https://veganpower.netlify.app"
+              linkGithub="https://github.com/vniacsta/veganpower-vanilla-website"
+              />
+          </td>
+          <td>
+            <ProjectsInfo
+              image={weatherApp}
+              linkImage="https://react-app-project-vniacsta.netlify.app/"
+              name="Weather App React"
+              linkProject="https://react-app-project-vniacsta.netlify.app/"
+              linkGithub="https://github.com/vniacsta/react-app-project"
+              />
+          </td>
+          <td>
+            <ProjectsInfo
+              image={quizApp}
+              linkImage="https://quiz-app-javascript-vniacsta.netlify.app/"
+              name="Quiz App Javascript"
+              linkProject="https://quiz-app-javascript-vniacsta.netlify.app/"
+              linkGithub="https://github.com/vniacsta/js-quiz-lordoftherings"
+              />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <ProjectsInfo
+              image={calcularhoras}
+              linkImage="https://calcularhoras.pt"
+              name="Calcular Horas"
+              linkProject="https://calcularhoras.pt"
+              linkGithub="https://github.com/vniacsta/calculate-hours"
+              />
+          </td> 
+          <td>
+            <ProjectsInfo
+              image={aecrru}
+              linkImage="https://www.aecrru.pt/"
+              name="AECRRU | Construction company website"
+              linkProject="https://www.aecrru.pt/"
+              linkGithub="https://github.com/vniacsta/js-website-aecrru"
+              />
+          </td> 
+        </tr>
+      </table>
     </div>
   );
 }
