ab600f0024e57e93f2ae9d01807b07836b759441
Vania Costa <vniacsta@users.noreply.github.com>
2020-12-08 23:04:36 +0000

    Update Certificates.js

--- src/Certificates.js
+++ src/Certificates.js
@@ -17,6 +17,32 @@ export default class SwipeToSlide extends Component {
       autoplaySpeed: 2500,
       slidesToShow: 3,
       swipeToSlide: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
     return (
       <div className="Projects">
