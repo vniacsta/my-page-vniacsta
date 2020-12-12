0e1903161a8d3807d3e4b8d7feafbce8c1f8ec3b
Vania Costa <vniacsta@users.noreply.github.com>
2020-12-12 16:13:02 +0000

    Added certificate

--- src/Certificates.js
+++ src/Certificates.js
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import "./Projects.css";
 import ProjectsInfo from "./ProjectsInfo";
 import certificate1 from "./media/codecademy-js.png";
+import certificate4 from "./media/codecademy-phaser.png";
 import certificate2 from "./media/certificate-plus.png";
 import certificate3 from "./media/certificate-react.png";
 
@@ -58,6 +59,14 @@ export default class SwipeToSlide extends Component {
           </div>
           <div>
             <ProjectsInfo
+              image={certificate4}
+              linkImage="https://www.codecademy.com/profiles/vniacsta/certificates/5ddbff3dd1bd3b001397ae9f"
+              name="Codecademy Javascript Certificate"
+              linkProject="https://www.codecademy.com/profiles/vniacsta/certificates/5ddbff3dd1bd3b001397ae9f"
+              />
+          </div>
+          <div>
+            <ProjectsInfo
               image={certificate2}
               linkImage="https://www.shecodes.io/certificates/40b6e552c908d683be92506fe52494c4"
               name="SheCodes Plus"
@@ -73,9 +82,6 @@ export default class SwipeToSlide extends Component {
               linkProject="https://react-app-project-vniacsta.netlify.app"
               linkGithub="https://github.com/vniacsta/react-app-project"
             />
-          </div>
-          <div>
-            
           </div>
         </Slider>
       </div>
